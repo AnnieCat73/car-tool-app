90ab4ccc6c2628d03efec55acd1ccfc9e75f1d57
Annie  Indreiten <devannie777@gmail.com>
2022-06-24 07:07:33 +0100

    Form functionality

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import React from 'react';
 
-export const CarTool = ({ cars, cats }) => {
+export const CarTool = ({ cars: initialCars }) => {
 
+  const [carForm, setCarForm] = useState({
+    make: '',
+    model: '',
+    year: 1900,
+    color: '',
+    price: 0,
+  });
+
+  const [cars, setCars] = useState(initialCars.concat())
+
+  const change = (e) => {
+    setCarForm({
+      ...carForm,
+      [e.target.name]: e.target.type === 'number'
+        ? Number(e.target.value)
+        : e.target.value,
+    });
+  };
+
+  const addCar = () => {
+    setCars(cars.concat({
+      ...carForm,
+      id: Math.max(...cars.map(c => c.id)) + 1,
+    })); //to get a unique id
+
+    setCarForm({
+      make: '',
+      model: '',
+      year: 1900,
+      color: '',
+      price: 0,
+    });//to initialise the form back to its original state*/
+  };
 
   return (
-    <>
+    <div style={{
+      display: 'flex'
+    }}>
       <header>
         <h1>Car Tool</h1>
       </header>
@@ -33,6 +69,35 @@ export const CarTool = ({ cars, cats }) => {
 
         </tbody>
       </table>
-    </>
+      <form>
+        <div>
+          <label htmlFor="make-input">Make:</label>
+          <input type="text" id="make-input" name="make"
+            value={carForm.make} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="model-input">Model:</label>
+          <input type="text" id="model-input" name="model"
+            value={carForm.model} onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="year-input">Year:</label>
+          <input type="number" id="year-input" name="year"
+            value={carForm.year} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="color-input">Color:</label>
+          <input type="text" id="color-input" name="color"
+            value={carForm.color} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="price-input">Price:</label>
+          <input type="number" id="price-input" name="price"
+            value={carForm.price} onChange={change} />
+        </div>
+        <button onClick={addCar}>Add Car</button>
+      </form>
+    </div >
   );
 };
